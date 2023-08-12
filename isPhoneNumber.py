4d6b9774e154540e0e11ec08555f18985c85c1e7
Kwadwo Bempah <kwadwoasiedu@bempah.com>
2023-08-11 22:40:31 -0400

    added regex matching to isPhoneNumber.py

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -1,33 +1,18 @@
+import re
+
 def isPhoneNumber(text:str)->bool:
-    if len(text) != 12: # if there aren't 12 digits
-        return False
-    index = 0
-    while index < 3: # first 3 digits
-        if not text[index].isdecimal():
-            return False
-        index += 1
-    if text[index] != '-': # delimiter
-        return False
-    index += 1
-    while index < 7: # middle 3 digits
-        if not text[index].isdecimal():
-            return False
-        index += 1
-    if text[index] != '-': # delimiter
-        return False
-    index += 1
-    while index < 12:
-        if not text[index].isdecimal():
-            return False
-        index += 1
-    return True
+    phoneNumberRegex = re.compile(r'\d{3}-\d{3}-\d{4}') # compile is passed a raw string indicating the type of pattern we want to match, returns Regex object
+    mo = phoneNumberRegex.search(text) # Regex object's search method is passed text to be searched and returns the first instance of the matching pattern found.
+    if mo != None:
+        return mo.group()
+    else:
+        return 'match not found'
 
 if __name__ == '__main__':
-    message = 'Call me at 978-427-8696 tomorrow. 978-654-0867 is my office.'
-    
-    for i in range(len(message)):
-        chunk = message[i:i+12]
-        if isPhoneNumber(chunk):
-            print('Phone number found:', chunk)
-            
+    message = 'My phone number is'
+
+    result = isPhoneNumber(message)
+
+    print(f'Phone number found: {result}')
+
     print('Done')
