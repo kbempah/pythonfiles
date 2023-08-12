d8e217c61f9cd6d7f7dea5266d0cc52a4a9a78f1
Kwadwo Bempah <kwadwoasiedu@bempah.com>
2023-08-11 23:23:17 -0400

    experimented with group matching

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -1,18 +1,12 @@
 import re
 
-def isPhoneNumber(text:str)->bool:
-    phoneNumberRegex = re.compile(r'\d{3}-\d{3}-\d{4}') # compile is passed a raw string indicating the type of pattern we want to match, returns Regex object
-    mo = phoneNumberRegex.search(text) # Regex object's search method is passed text to be searched and returns the first instance of the matching pattern found.
-    if mo != None:
-        return mo.group()
-    else:
-        return 'match not found'
-
 if __name__ == '__main__':
-    message = 'My phone number is'
+    message = 'My phone number is 978-427-8696.'
 
-    result = isPhoneNumber(message)
+    phoneNumberRegex = re.compile(r'(\d{3})-(\d{3}-\d{4})') # compile is passed a raw string indicating the type of pattern we want to match, returns Regex object
+    mo = phoneNumberRegex.search(message) # Regex object's search method is passed text to be searched and returns the first instance of the matching pattern found.
 
-    print(f'Phone number found: {result}')
-
-    print('Done')
+    print(mo.group()) # matches and returns whole pattern
+    print(mo.group(1)) # matches and returns first group enclosed in ()
+    print(mo.group(2)) # matches and returns second group enclosed in () and so on...
+    print(mo.groups()) # matches and returns all groups as a tuple
